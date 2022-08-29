ac525261de81ab63c378c3332e296bd94a7806c7
BohdanSereda <zxysereda.bodik@gmail.com>
2022-08-29 19:57:55 +0300

    DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ScraperModule } from './scraper/scraper.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ScraperModule],
+  imports: [ScraperModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      synchronize: true,
+      autoLoadEntities: true
+    })],
   controllers: [],
   providers: [],
 })

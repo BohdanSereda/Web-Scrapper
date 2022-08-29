c511097382769da97db5c295ef67683ab5c47e36
BohdanSereda <zxysereda.bodik@gmail.com>
2022-08-30 01:03:01 +0300

    update scraper module

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
 import { ScraperController } from './scraper.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Business } from './entities/business.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Business])],
   controllers: [ScraperController],
   providers: [ScraperService]
 })

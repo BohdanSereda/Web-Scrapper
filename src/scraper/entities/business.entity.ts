6e08c8a8c77134c372c5cc7aab4219989afe3ecb
BohdanSereda <zxysereda.bodik@gmail.com>
2022-08-30 01:00:39 +0300

    create business entity

--- src/scraper/entities/business.entity.ts
+++ src/scraper/entities/business.entity.ts
@@ -1,6 +1,37 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Restaurant{
+export class Business{
+    @PrimaryGeneratedColumn()
+    id: number
 
+    @Column()
+    name: string
+
+    @Column("longtext", {nullable: true})
+    description: string
+
+    @Column("simple-array")
+    images: string[]
+
+    @Column()
+    address: string
+
+    @Column({nullable: true})
+    phone: string
+
+    @Column()
+    rating: number
+
+    @Column("longtext")
+    lowest_rated_review: string
+
+    @Column("longtext")
+    highest_rated_review: string
+
+    @Column("simple-array")
+    amenities: string[]
+
+    @Column("simple-array")
+    workingHours: string[]
 }

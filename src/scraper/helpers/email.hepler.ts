eb4e3597fbd71ab5b7f52a1b2646cf35ccde6306
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-01 13:24:21 +0300

    update email helper

--- src/scraper/helpers/email.hepler.ts
+++ src/scraper/helpers/email.hepler.ts
@@ -1,12 +1,12 @@
 import { MailerService } from "@nestjs-modules/mailer";
 
 export class EmailHelper{
-    static async sendEmail(mailerService: MailerService, email: string){
+    static async sendEmail(mailerService: MailerService, email:string, text:string){
         await mailerService.sendMail({
             to: email,
             from: 'scraper.api.study@gmail.com',
-            subject: 'Scraping finished',
-            text: 'Scraping finished'
+            subject: 'Scraping',
+            text
         })
     }
 }

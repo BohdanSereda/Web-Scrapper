68a00d4487224b2ca43bc3d5e452f8e8bc938794
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-01 13:21:25 +0300

    fixed bug with incorrect response

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,18 +1,20 @@
 
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Query, Res } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { GetBusinessDto } from './dto/get-business.dto';
+import { scrapeBusinessDto } from './dto/scrape-business.dto';
 import { ScraperService } from './scraper.service';
 
 @Controller('scraper')
 export class ScraperController {
   constructor(private readonly scraperService: ScraperService) { }
 
-  @Post('/:city/:email')
-  @ApiResponse({status: 200, description: 'Scrape business data from first 5 pages and post unique data in data base', type: [GetBusinessDto]})
+  @Post()
+  @ApiResponse({status: 200, description: 'Scrape business data from first 5 pages and post unique data in data base', type: String })
   @ApiResponse({ status: 500, description: 'Internal server error.'})
-  scrapeBusinessData(@Param('city') city: string, @Param('email') email: string): Promise<GetBusinessDto[]> {
-    return this.scraperService.scrapeBusinessData(city, email);
+  scrapeBusinessData(@Body() body: scrapeBusinessDto, @Res() res) {
+    res.send({message:'scraping in progress, you will be notified via email'});
+    this.scraperService.scrapeBusinessData(body.city, body.email)
   }
 
   @Get()
--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CheerioAPI, load } from 'cheerio';
-import { info } from 'console';
 import { Repository } from 'typeorm';
 import { GetBusinessDto } from './dto/get-business.dto';
 
@@ -12,6 +11,7 @@ import { DataBaseHelper } from './helpers/db.helper';
 import { EmailHelper } from './helpers/email.hepler';
 import { InformationScraperHelper } from './helpers/information-scraper.helper';
 import { PageScraperHelper } from './helpers/page-scraper.helper'
+import { TimerHelper } from './helpers/timer.helper';
 
 @Injectable()
 export class ScraperService {
@@ -27,37 +27,42 @@ export class ScraperService {
         return informationScraperHelper.scrapeLinks()
     }
 
-    async scrapeBusinessData(city: string, email: string): Promise<GetBusinessDto[]> {
+    async scrapeBusinessData(city: string, email: string): Promise<string> {
         console.log(email, typeof email)
         console.time('performance')
         let businessesLinks = []
         const pageScraperHelper = new PageScraperHelper()
-
-        for (let i = 0; i < 50; i += 10) {
-            const link = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc=' + city + '&start=' + i
-            const businessListPageHtml = await pageScraperHelper.scrapePage(link)
-            businessesLinks = businessesLinks.concat(this.getBusinessLinks(businessListPageHtml))
-        }
-        const businessesData = []
-        for (const businessesLink of businessesLinks) {
-            const businessLink = 'https://www.yelp.com' + businessesLink + '&sort_by=rating_asc'
-            await pageScraperHelper.timer(1000)
-            const businessPageHtml = await pageScraperHelper.scrapePage(businessLink)
-            const $: CheerioAPI = load(businessPageHtml);
-            const informationScraperHelper = new InformationScraperHelper($)
-            const business = await informationScraperHelper.extractBusinessPageInformation(businessLink, city)
-            const savedBusiness = await DataBaseHelper.createUniqueBusiness(business, this.businessRepository)
-            if(!savedBusiness){
-                continue
-            }else{
-                businessesData.push(savedBusiness)
+        try {
+            for (let i = 0; i < 50; i += 10) {
+                const link = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc=' + city + '&start=' + i
+                await TimerHelper.timer(5000)
+                const businessListPageHtml = await pageScraperHelper.scrapePage(link)
+                businessesLinks = businessesLinks.concat(this.getBusinessLinks(businessListPageHtml))
             }
+            const businessesData = []
+            for (const businessesLink of businessesLinks) {
+                const businessLink = 'https://www.yelp.com' + businessesLink + '&sort_by=rating_asc'
+                await TimerHelper.timer(5000)
+                const businessPageHtml = await pageScraperHelper.scrapePage(businessLink)
+                const $: CheerioAPI = load(businessPageHtml);
+                const informationScraperHelper = new InformationScraperHelper($)
+                const business: GetBusinessDto = await informationScraperHelper.extractBusinessPageInformation(businessLink, city)
+                const savedBusiness = await DataBaseHelper.createUniqueBusiness(business, this.businessRepository)
+                if(!savedBusiness){
+                    continue
+                }else{
+                    businessesData.push(savedBusiness)
+                }
+            }
+            console.timeEnd('performance')
+            console.log(`scraped ${businessesData.length} businesses`);
+            await EmailHelper.sendEmail(this.mailerService, email, `Successfully scraped ${businessesData.length} businesses`)
+            return 'done'
+        } catch (error) {
+            console.error(error)
+            await EmailHelper.sendEmail(this.mailerService, email, `Scraping error: something went wrong :(`)
         }
 
-        await EmailHelper.sendEmail(this.mailerService, email)
-        console.timeEnd('performance')
-        console.log(`scraped ${businessesData.length} businesses`);
-        return businessesData
     }
 
     async getBusinesses(city: string){

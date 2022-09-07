6eeef8eeed53378dd8641974ae7b12c723b51f7a
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-07 18:14:30 +0300

    refactor reservation service

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -3,44 +3,35 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Reservation } from './entities/reservation.entity';
-import {DataBaseHelper} from '../helpers/db.helper';
-import { EmailHelper } from './helpers/email.hepler';
+import { DataBaseHelper } from '../helpers/db.helper';
+import { EmailHelper } from '../helpers/email.helper';
 import { UpdateReservationsStatusDto } from './dto/update-reservation-status.dto';
 @Injectable()
 export class ReservationService {
-    constructor(        
+    constructor(
         @InjectRepository(Reservation)
         private readonly reservationRepository: Repository<Reservation>,
         private readonly mailerService: MailerService
-        ){}
+    ) { }
 
-    async createReservation(email: string, date: string, time: string, userName: string){
-       return DataBaseHelper.createReservation({email, date, time, userName}, this.reservationRepository)
+    async createReservation(email: string, date: string, time: string, userName: string) {
+        return DataBaseHelper.createReservation({ email, date, time, userName }, this.reservationRepository)
     }
 
-    async getAllPendingReservations(){
-        
+    async getAllPendingReservations() {
         return DataBaseHelper.getAllPendingReservations(this.reservationRepository)
     }
 
-    async updateReservationsStatus(id: string, updateReservationsStatus: UpdateReservationsStatusDto){
-        const status = updateReservationsStatus.status
-        if(status !== 'confirmed' && status !== 'declined' && status !== 'pending'){
+    async updateReservationsStatus(id: string, updateReservationsStatus: UpdateReservationsStatusDto) {
+        const emailHelper = new EmailHelper()
+        const updateReservation = await DataBaseHelper.updateReservationsStatus(id, updateReservationsStatus, this.reservationRepository)
+        if (!updateReservation) {
             throw new HttpException({
                 status: HttpStatus.BAD_REQUEST,
-                error: 'incorrect status value status field must have values: "pending", "declined", "confirmed"',
+                error: 'bad request',
             }, HttpStatus.BAD_REQUEST)
         }
-        const updateReservation = await DataBaseHelper.updateReservationsStatus(id, updateReservationsStatus, this.reservationRepository)
-        if(updateReservation){
-            await EmailHelper.sendEmail(this.mailerService, updateReservation)
-            return updateReservation
-        }else{
-            throw new HttpException({
-                status: HttpStatus.NOT_FOUND,
-                error: 'can\'t find reservation',
-            }, HttpStatus.NOT_FOUND)
-
-        }
+        await emailHelper.sendReservationEmail(this.mailerService, updateReservation)
+        return updateReservation
     }
 }

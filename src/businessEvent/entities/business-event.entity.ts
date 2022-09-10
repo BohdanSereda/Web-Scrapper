1b57ac2e821ac8015545fa59ca3af72b1eeb50f8
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-10 12:28:09 +0300

    update business event entity

--- src/businessEvent/entities/business-event.entity.ts
+++ src/businessEvent/entities/business-event.entity.ts
@@ -1,9 +1,29 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { StatusType } from '../dto/custom-types';
 
 @Entity()
 export class BusinessEvent{
     @PrimaryGeneratedColumn()
     id: number
 
+    @Column()
+    name: string
+
+    @Column()
+    event_start: string
+
+    @Column()
+    event_end: string
+
+    @Column("longtext")
+    description: string
+
+    @Column()
+    image: string 
+
+    @Column()
+    features: string
+
+    @Column()
+    frequency: StatusType
 }

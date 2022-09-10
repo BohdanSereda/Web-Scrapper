d26f0a0522c96bc07f97bf872fdc8d3f052df972
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-10 12:27:24 +0300

    update create business event dto

--- src/businessEvent/dto/create-business-event.dto.ts
+++ src/businessEvent/dto/create-business-event.dto.ts
@@ -1,3 +1,35 @@
-export class CreateBusinessEventDto {
+import { ApiProperty } from "@nestjs/swagger"
+import { IsString, IsNotEmpty, IsEmail, IsIn } from "class-validator"
+import { StatusType } from "./custom-types"
 
+export class CreateBusinessEventDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({type: String, description: 'event name'})
+    readonly name: string
+
+    @IsString()
+    @ApiProperty({type: String, description: 'event start time'})
+    readonly event_start: string
+
+    @IsString()
+    @ApiProperty({type: String, description: 'event end time'})
+    readonly event_end: string
+
+    @IsString()
+    @ApiProperty({type: String, description: 'event description'})
+    readonly description: string
+    
+    @IsString()
+    @ApiProperty({type: String, description: 'event image'})
+    readonly image: string
+
+    @IsString()
+    @ApiProperty({type: String, description: 'event features'})
+    readonly features: string
+
+    @IsString()
+    @IsIn(['daily', 'weekly', 'monthly', 'yearly', ''])
+    @ApiProperty({type: "StatusType", description: 'event frequency'})
+    frequency?: StatusType
 }
---
+++ src/businessEvent/dto/custom-types.ts
@@ -0,0 +1,1 @@
+export type StatusType = 'daily'| 'weekly'| 'monthly' | 'yearly'| ''

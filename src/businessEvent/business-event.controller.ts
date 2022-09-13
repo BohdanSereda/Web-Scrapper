8d82e85cee692b9e1e440f399069c06ee27d097f
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-13 17:14:14 +0300

    update business event controller

--- src/businessEvent/business-event.controller.ts
+++ src/businessEvent/business-event.controller.ts
@@ -1,13 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, FileTypeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiResponse } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import path = require('path');
+import { v4 as uuidv4 } from 'uuid';
 import { BusinessEventService } from './business-event.service';
 import { CreateBusinessEventDto } from './dto/create-business-event.dto';
+import { fileFilter } from './dto/custom-types';
 
 @Controller('business-event')
 export class BusinessEventController {
   constructor(private readonly businessEventService: BusinessEventService) {}
   
   @Post()
+  @UseInterceptors(FileInterceptor('image',{
+    storage: diskStorage({
+      destination: './images',
+      filename: (req, file, cb) =>{
+        const fileExtension: string = path.extname(file.originalname);
+        const fileName: string = uuidv4() + fileExtension
+        cb(null, fileName)
+      }
+    })
+  }))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        event_start: { type: 'integer' },
+        event_end : {type: 'string'},
+        description : {type: 'string'},
+        image: {
+          type: 'string',
+          format: 'binary',
+        },
+        features : {type: 'string'},
+        frequency: {type: 'string'},
+        businessId: {type: 'number'}
+      },
+    },
+  })
   @ApiResponse({status: 201, description: `Create new event. 
   \nATTENTION: 
   \nfrequency should be  "daily " or "weekly" or "" it determines frequency of event "" - one time event 
@@ -15,7 +49,16 @@ export class BusinessEventController {
   \nevent_end example: 13.09.2022:13:30` })
   @ApiResponse({ status: 400, description: 'bad request.'})
   @ApiResponse({ status: 500, description: 'internal server error.'})
-  createEvent(@Body() createBusinessEventDto: CreateBusinessEventDto){
-    return this.businessEventService.createEvent(createBusinessEventDto)
+  createEvent(
+    @Body() createBusinessEventDto: CreateBusinessEventDto, 
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({fileType: fileFilter})
+        ]
+      })
+    )
+    image){
+    return this.businessEventService.createEvent(createBusinessEventDto, image)
   }
 }

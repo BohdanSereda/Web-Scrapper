e053a4d01e9409319e70f924e6b67125b5bf24b6
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-13 18:30:48 +0300

    update twitter module

--- src/twitter/twitter.module.ts
+++ src/twitter/twitter.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TwitterService } from './twitter.service';
-import { TwitterController } from './twitter.controller';
 
 @Module({
-  controllers: [TwitterController],
-  providers: [TwitterService]
+  providers: [TwitterService],
+  exports: [TwitterService]
 })
 export class TwitterModule {}

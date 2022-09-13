d4a743b9e710d0a006f40b58da32d78a362d7450
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-13 04:15:05 +0300

    update business event service

--- src/businessEvent/business-event.service.ts
+++ src/businessEvent/business-event.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataBaseHelper } from 'src/helpers/db.helper';
+import { Business } from 'src/scraper/entities/business.entity';
 import { Repository } from 'typeorm';
 import { CreateBusinessEventDto } from './dto/create-business-event.dto';
 import { BusinessEvent } from './entities/business-event.entity';
+import { BusinessEventValidator } from './helpers/validation.helper';
 
 @Injectable()
 export class BusinessEventService {
     constructor(
         @InjectRepository(BusinessEvent)
-        private readonly reservationRepository: Repository<BusinessEvent>,
+        private readonly businessEventRepository: Repository<BusinessEvent>,
+        @InjectRepository(Business)
+        private readonly businessRepository: Repository<Business>,
     ) { }
+
     async createEvent(createBusinessEventDto: CreateBusinessEventDto) {
-        return DataBaseHelper
+        return await BusinessEventValidator.dateValidation(createBusinessEventDto, this.businessEventRepository, this.businessRepository)
+    }
+
+    async getBusinessEvents(createBusinessEventDto: CreateBusinessEventDto){
+        return DataBaseHelper.getBusinessEvents(createBusinessEventDto, this.businessEventRepository, this.businessRepository)
     }
 }

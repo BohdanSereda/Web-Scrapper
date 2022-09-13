e849801d8f941e912eb06cef4249f0aefffca410
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-13 18:31:05 +0300

    create twitter service main logic

--- src/twitter/twitter.service.ts
+++ src/twitter/twitter.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { TwitterHelper } from './helpers/twitter.helper';
+import * as fsExtra from 'fs-extra'
+import { BusinessEvent } from 'src/businessEvent/entities/business-event.entity';
 @Injectable()
-export class TwitterService {}
+export class TwitterService {    
+    async postTweet(event, image){
+        try {
+            const rwClient = TwitterHelper.twitterGetClient()
+            const media = await Promise.all([rwClient.v1.uploadMedia(image.path)])
+            await rwClient.v1.tweet(
+                `${event.name},
+                \n${event.description}
+                \nfrom: ${event.event_start} till: ${event.event_end} ${event.frequency}
+                \n${event.features} `, 
+                {media_ids: media}
+                )
+            fsExtra.emptyDir('images')
+        } catch (error) {
+            return error
+        }     
+    }
+}

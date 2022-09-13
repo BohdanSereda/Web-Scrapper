02bc532b56c2eec49b53dcb8ce3feb43fa791cac
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-13 18:31:16 +0300

    create twitter helper main logic

---
+++ src/twitter/helpers/twitter.helper.ts
@@ -0,0 +1,13 @@
+import {TwitterApi} from "twitter-api-v2";
+
+export class TwitterHelper {
+    static twitterGetClient(){
+        const client = new TwitterApi({
+            appKey: process.env.TWITTER_API_KEY,
+            appSecret: process.env.TWITTER_SECRET_KEY,
+            accessToken: process.env.TWITTER_ACCESS_TOKEN,
+            accessSecret: process.env.TWITTER_SECRET_TOKEN
+        })
+        return client.readWrite
+    }
+}

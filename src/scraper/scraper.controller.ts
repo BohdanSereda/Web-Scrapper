0e0e7e5d6a44bde552e01032772c89e41e19f0d7
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-14 12:27:24 +0300

    update documentation and refactor

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,35 +1,40 @@
 
-import { Body, Controller, Get, HttpStatus, Param, Post, Query, Res } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
-import { GetBusinessDto } from './dto/get-business.dto';
-import { scrapeBusinessDto } from './dto/scrape-business.dto';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import {  ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { ScrapeBusinessDto } from './dto/scrape-business.dto';
 import { TimerHelper } from './helpers/timer.helper';
 import { ScraperService } from './scraper.service';
-
+import { Response } from "express";
+import { Business } from './entities/business.entity';
 @Controller('scraper')
 export class ScraperController {
   constructor(private readonly scraperService: ScraperService) { }
 
   @Post()
-  @ApiResponse({status: 201, description: 'Scrape business data from first 5 pages and post unique data in data base'})
-  @ApiResponse({ status: 500, description: 'Internal server error.'})
-  async scrapeBusinessData(@Body() body: scrapeBusinessDto, @Res() res) {
+  @ApiResponse({ status: 201, description: 'Scrape business data from first 5 pages and post unique data in data base'})
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  async scrapeBusinessData(@Body() scrapeBusinessDto: ScrapeBusinessDto, @Res() res: Response): Promise<Response> {
     const result = await Promise.race([
-      this.scraperService.scrapeBusinessData(body.city, body.email),
+      this.scraperService.scrapeBusinessData(scrapeBusinessDto),
       TimerHelper.raceTimer(false, 6000)
     ])
-    if(!result){
-      res.send({message:'scraping in progress, you will be notified via email'});
+    if (!result) {
+      return res.send({ message: 'scraping in progress, you will be notified via email' });
     }
-    if(result){
-      res.send({message: result});
-    }
+    return res.send({ message: result });
+
   }
 
   @Get()
-  @ApiResponse({status: 200, description: 'Get all business data or by business city from data base',  type: [GetBusinessDto]})
-  @ApiResponse({ status: 500, description: 'Internal server error.'})
-  getBusinesses(@Query('city') city: string): Promise<GetBusinessDto[]> {
+  @ApiQuery({
+    name: 'city',
+    required: false
+  })
+  @ApiResponse({ status: 200, description: 'Get all business data or by business city from data base'})
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  getBusinesses(@Query('city') city: string): Promise<Business[]> {
     return this.scraperService.getBusinesses(city)
   }
 }

910053313322a73d13369dd02445c995eeb70cca
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-14 12:28:37 +0300

    update dto

--- src/scraper/dto/scrape-business.dto.ts
+++ src/scraper/dto/scrape-business.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
-export class scrapeBusinessDto {
+export class ScrapeBusinessDto {
     @IsString()
     @IsNotEmpty()
     @ApiProperty({type: String, description: 'business city'})

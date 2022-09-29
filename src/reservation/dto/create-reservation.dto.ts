19540b7f72fb8306e254f38c7c4cbf7ef28f5ae6
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-30 00:06:36 +0300

    update reservation documentation

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,28 +1,45 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsIn, IsNotEmpty, IsString } from 'class-validator'
+import { IsEmail, IsIn, IsNotEmpty, IsString } from 'class-validator';
 import { StatusType } from './custom-types';
 
 export class CreateReservationDto {
-    @IsString()
-    @IsNotEmpty()
-    @IsEmail()
-    @ApiProperty({type: String, description: 'user email name'})
-    readonly email: string
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty({
+    example: 'bob@gmail.com',
+    type: String,
+    description: 'user email name',
+  })
+  readonly email: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'user name'})
-    readonly userName: string
+  @IsString()
+  @ApiProperty({ example: 'bob', type: String, description: 'user name' })
+  readonly userName: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'reservation date'})
-    readonly date: string
+  @IsString()
+  @ApiProperty({
+    example: 'September 8, 2023',
+    type: String,
+    description: 'reservation date',
+  })
+  readonly date: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'reservation time'})
-    readonly time: string
+  @IsString()
+  @ApiProperty({
+    example: '06:00 PM',
+    type: String,
+    description: 'reservation time',
+  })
+  @ApiProperty({ type: String, description: 'reservation time' })
+  readonly time: string;
 
-    @IsString()
-    @IsIn(['confirmed', 'declined', 'pending'])
-    @ApiProperty({type: "StatusType", description: 'reservation status'})
-    status?: StatusType
+  @IsString()
+  @IsIn(['confirmed', 'declined', 'pending'])
+  @ApiProperty({
+    example: 'confirmed',
+    type: 'StatusType',
+    description: 'reservation status',
+  })
+  status?: StatusType;
 }
--- src/reservation/entities/reservation.entity.ts
+++ src/reservation/entities/reservation.entity.ts
@@ -1,24 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { StatusType } from '../dto/custom-types';
 
 @Entity()
-export class Reservation{
-    @PrimaryGeneratedColumn()
-    id: number
+export class Reservation {
+  @ApiProperty({ example: 1, type: Number, description: 'reservation id' })
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string
+  @ApiProperty({
+    example: 'bob@gmail.com',
+    type: String,
+    description: 'user email',
+  })
+  @Column()
+  email: string;
 
-    @Column()
-    userName: string
+  @ApiProperty({ example: 'bob', type: String, description: 'user name' })
+  @Column()
+  userName: string;
 
-    @Column()
-    date: string
+  @ApiProperty({
+    example: 'September 8, 2023',
+    type: String,
+    description: 'reservation date',
+  })
+  @Column()
+  date: string;
 
-    @Column()
-    time: string
+  @ApiProperty({
+    example: '06:00 PM',
+    type: String,
+    description: 'reservation time',
+  })
+  @Column()
+  time: string;
 
-    @Column()
-    status: StatusType
-
+  @ApiProperty({
+    example: 'confirmed',
+    type: String,
+    description: 'reservation status',
+  })
+  @Column()
+  status: StatusType;
 }

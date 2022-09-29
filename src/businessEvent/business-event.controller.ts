c26ea6bc9bb6957abf208be1bb1af16f3ef52d21
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-30 00:04:53 +0300

    refactor and update documentation

--- src/businessEvent/business-event.controller.ts
+++ src/businessEvent/business-event.controller.ts
@@ -1,73 +1,66 @@
-import { Body, Controller, FileTypeValidator, Param, ParseFilePipe, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiResponse } from '@nestjs/swagger';
-import { diskStorage } from 'multer';
-import path = require('path');
-import { v4 as uuidv4 } from 'uuid';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { BusinessEventService } from './business-event.service';
 import { CreateBusinessEventDto } from './dto/create-business-event.dto';
-import { fileFilter } from './dto/custom-types';
 import { BusinessEvent } from './entities/business-event.entity';
+import {
+  apiBodySchema,
+  fileInterceptorConfig,
+  parseFile,
+} from './helpers/api-post.helper';
 
+@ApiTags('Business events')
 @Controller('business-event')
 export class BusinessEventController {
   constructor(private readonly businessEventService: BusinessEventService) {}
-  
-  @Post()
-  @UseInterceptors(FileInterceptor('image',{
-    storage: diskStorage({
-      destination: './images',
-      filename: (req, file, cb) =>{
-        const fileExtension: string = path.extname(file.originalname);
-        const fileName: string = uuidv4() + fileExtension
-        cb(null, fileName)
-      }
-    })
-  }))
+
+  @UseInterceptors(FileInterceptor('image', fileInterceptorConfig))
   @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-        event_start: { type: 'integer' },
-        event_end : {type: 'string'},
-        description : {type: 'string'},
-        image: {
-          type: 'string',
-          format: 'binary',
-        },
-        features : {type: 'string'},
-        frequency: {type: 'string'},
-        businessId: {type: 'number'}
-      },
-    },
+  @ApiBody(apiBodySchema)
+  @ApiOperation({ summary: 'Business event creation' })
+  @ApiResponse({
+    status: 201,
+    description: `Create new event.`,
+    type: BusinessEvent,
   })
-  @ApiResponse({status: 201, description: `Create new event. 
-  \nATTENTION: 
-  \nfrequency should be  "daily " or "weekly" or "" it determines frequency of event "" - one time event 
-  \nevent_start example: 13.09.2022:9:00
-  \nevent_end example: 13.09.2022:13:30` })
-  @ApiResponse({ status: 400, description: 'bad request.'})
-  @ApiResponse({ status: 500, description: 'internal server error.'})
+  @ApiResponse({ status: 400, description: 'bad request.' })
+  @ApiResponse({ status: 500, description: 'internal server error.' })
+  @Post()
   async createEvent(
-    @Body() createBusinessEventDto: CreateBusinessEventDto, 
-    @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new FileTypeValidator({fileType: fileFilter})
-        ]
-      })
-    )
-    image: Express.Multer.File): Promise<false | BusinessEvent>{
-    return this.businessEventService.createEvent(createBusinessEventDto, image)
+    @Body() createBusinessEventDto: CreateBusinessEventDto,
+    @UploadedFile(parseFile)
+    image: Express.Multer.File,
+  ): Promise<false | BusinessEvent> {
+    return this.businessEventService.createEvent(createBusinessEventDto, image);
   }
 
-  @ApiResponse({status: 201, description: `increment user counter.`})
-  @ApiResponse({ status: 404, description: 'not found.'})
-  @ApiResponse({ status: 500, description: 'internal server error.'})
+  @ApiOperation({ summary: 'Increment user counter' })
+  @ApiResponse({
+    status: 201,
+    description: `increment user counter.`,
+    type: BusinessEvent,
+  })
+  @ApiResponse({ status: 404, description: 'not found.' })
+  @ApiResponse({ status: 500, description: 'internal server error.' })
   @Patch(':id')
-  async incrementUserCounter(@Param('id') businessEventId: string): Promise<BusinessEvent>{
-    return this.businessEventService.incrementUserCounter(businessEventId)
+  async incrementUserCounter(
+    @Param('id') businessEventId: string,
+  ): Promise<BusinessEvent> {
+    return this.businessEventService.incrementUserCounter(businessEventId);
   }
 }

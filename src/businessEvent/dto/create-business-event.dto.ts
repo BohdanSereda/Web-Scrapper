593918ecd545ad050a69838ded4e8655b0f6876e
BohdanSereda <zxysereda.bodik@gmail.com>
2022-09-30 00:05:30 +0300

    formate

--- src/businessEvent/dto/create-business-event.dto.ts
+++ src/businessEvent/dto/create-business-event.dto.ts
@@ -1,40 +1,46 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { IsString, IsNotEmpty, IsIn, IsNumber, IsOptional } from "class-validator"
-import { FrequencyType } from "./custom-types"
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsIn,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+import { FrequencyType } from './custom-types';
 
 export class CreateBusinessEventDto {
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({type: String, description: 'event name'})
-    readonly name: string
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'event name'})
+  readonly name: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'event start time'})
-    readonly event_start: string
+  @IsString()
+  @ApiProperty({ type: String, description: 'event start time' })
+  readonly event_start: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'event end time'})
-    readonly event_end: string
+  @IsString()
+  @ApiProperty({ type: String, description: 'event end time' })
+  readonly event_end: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'event description'})
-    readonly description: string
+  @IsString()
+  @ApiProperty({ type: String, description: 'event description' })
+  readonly description: string;
 
-    @IsString()
-    @ApiProperty({type: String, description: 'event features'})
-    readonly features: string
+  @IsString()
+  @ApiProperty({ type: String, description: 'event features' })
+  readonly features: string;
 
-    @IsString()
-    @IsIn(['daily', 'weekly', ''])
-    @ApiProperty({type: "FrequencyType", description: 'event frequency'})
-    frequency: FrequencyType
+  @IsString()
+  @IsIn(['daily', 'weekly', ''])
+  @ApiProperty({ type: 'FrequencyType', description: 'event frequency' })
+  frequency: FrequencyType;
 
-    @IsNumber()
-    @IsOptional()
-    visitorsCount?: number
+  @IsNumber()
+  @IsOptional()
+  visitorsCount?: number;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({type: String, description: 'event businessId'})
-    readonly businessId: number
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'event businessId' })
+  readonly businessId: number;
 }

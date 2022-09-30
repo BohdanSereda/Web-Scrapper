3d4bf12ea35343639e0dc4ac394c5fbdda0d8f34
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-01 01:30:38 +0300

    remove unnecessary imports

--- src/businessEvent/helpers/api-post.helper.ts
+++ src/businessEvent/helpers/api-post.helper.ts
@@ -2,8 +2,6 @@ import { ParseFilePipe, FileTypeValidator } from '@nestjs/common';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { ApiBodyOptions } from '@nestjs/swagger';
 import { diskStorage } from 'multer';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
 import { fileFilter } from '../dto/custom-types';
 
 export const parseFile: ParseFilePipe = new ParseFilePipe({

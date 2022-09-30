164be8e36f58ddaa40768ab07e03042ad4ed04d9
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-01 01:28:05 +0300

    create new custom type

--- src/businessEvent/dto/custom-types.ts
+++ src/businessEvent/dto/custom-types.ts
@@ -1,2 +1,15 @@
+import { BusinessEvent } from "../entities/business-event.entity";
+
 export type FrequencyType = 'daily'| 'weekly' | ''
 export const fileFilter = 'raw|png|jpg|jpeg|tiff|psd|bmp|gif'
+export type DateIntervalData = {
+    event: BusinessEvent;
+    normalized: {
+        normalizedStart: moment.Moment;
+        normalizedEnd: moment.Moment;
+        normalizedExistingStart: moment.Moment;
+        normalizedExistingEnd: moment.Moment;
+    };
+    context: FrequencyType;
+    coincided?: boolean;
+}

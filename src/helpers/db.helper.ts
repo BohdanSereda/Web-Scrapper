97a7514bb171b85c36539172ef5b98e79ee1b536
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-05 12:34:23 +0300

    formatted global helper

--- src/helpers/db.helper.ts
+++ src/helpers/db.helper.ts
@@ -1,108 +1,148 @@
-import { Repository } from "typeorm";
-import { GetBusinessDto } from "../scraper/dto/get-business.dto";
-import { Business } from "../scraper/entities/business.entity";
-import { CreateReservationDto } from "src/reservation/dto/create-reservation.dto";
-import { Reservation } from "src/reservation/entities/reservation.entity";
-import { UpdateReservationsStatusDto } from "src/reservation/dto/update-reservation-status.dto";
-import { BusinessEvent } from "src/businessEvent/entities/business-event.entity";
-import { CreateBusinessEventDto } from "src/businessEvent/dto/create-business-event.dto";
+import { Repository } from 'typeorm';
+import { GetBusinessDto } from '../scraper/dto/get-business.dto';
+import { Business } from '../scraper/entities/business.entity';
+import { CreateReservationDto } from 'src/reservation/dto/create-reservation.dto';
+import { Reservation } from 'src/reservation/entities/reservation.entity';
+import { UpdateReservationsStatusDto } from 'src/reservation/dto/update-reservation-status.dto';
+import { BusinessEvent } from 'src/businessEvent/entities/business-event.entity';
+import { CreateBusinessEventDto } from 'src/businessEvent/dto/create-business-event.dto';
 
 export class DataBaseHelper {
-    static async createUniqueBusiness(business: GetBusinessDto, businessRepository: Repository<Business>) {
-        const existBusiness = await businessRepository.findOne({ where: { name: business.name, city: business.city } })
+    static async createUniqueBusiness(
+        business: GetBusinessDto,
+        businessRepository: Repository<Business>,
+    ) {
+        const existBusiness = await businessRepository.findOne({
+            where: { name: business.name, city: business.city },
+        });
         if (existBusiness) {
-            return false
+            return false;
         }
-        const createdBusiness = businessRepository.create(business)
-        return businessRepository.save(createdBusiness)
+        const createdBusiness = businessRepository.create(business);
+        return businessRepository.save(createdBusiness);
     }
 
-    static async getBusinessesByCity(city: string, businessRepository: Repository<Business>) {
-        return await businessRepository.find({ where: { city } })
+    static async getBusinessesByCity(
+        city: string,
+        businessRepository: Repository<Business>,
+    ) {
+        return await businessRepository.find({ where: { city } });
     }
 
-    static async getBusinessesByID(id: string, businessRepository: Repository<Business>) {
-        return await businessRepository.findOneBy({id:+id})
+    static async getBusinessesByID(
+        id: string,
+        businessRepository: Repository<Business>,
+    ) {
+        return await businessRepository.findOneBy({ id: +id });
     }
 
-    static async getBusinesses(city: string, businessRepository: Repository<Business>) {
-        const query = city ? { where: { city } } : {}
-        return await businessRepository.find(query)
+    static async getBusinesses(
+        city: string,
+        businessRepository: Repository<Business>,
+    ) {
+        const query = city ? { where: { city } } : {};
+        return await businessRepository.find(query);
     }
-
 
     static async getCities(businessRepository: Repository<Business>) {
-        const rawCities = await businessRepository.createQueryBuilder().select("city").groupBy('city').execute();
-        return rawCities.map((rawCity: {city: string}) => rawCity.city)
+        const rawCities = await businessRepository
+            .createQueryBuilder()
+            .select('city')
+            .groupBy('city')
+            .execute();
+        return rawCities.map((rawCity: { city: string }) => rawCity.city);
     }
 
-    static async getRestaurant(restaurantName: string, businessRepository: Repository<Business>) {
-        return await businessRepository.findOne({ where: { name: restaurantName } })
+    static async getRestaurant(
+        restaurantName: string,
+        businessRepository: Repository<Business>,
+    ) {
+        return await businessRepository.findOne({
+            where: { name: restaurantName },
+        });
     }
 
     static async createReservation(
-        reservation: CreateReservationDto, 
-        reservationRepository: Repository<Reservation>) {
-
-        reservation.status = 'pending'
-        const createdBusiness = reservationRepository.create(reservation)
-        return reservationRepository.save(createdBusiness)
+        reservation: CreateReservationDto,
+        reservationRepository: Repository<Reservation>,
+    ) {
+        reservation.status = 'pending';
+        const createdBusiness = reservationRepository.create(reservation);
+        return reservationRepository.save(createdBusiness);
     }
 
-    static async getAllPendingReservations(reservationRepository: Repository<Reservation>) {
-        return await reservationRepository.find({ where: { status: 'pending' } })
+    static async getAllPendingReservations(
+        reservationRepository: Repository<Reservation>,
+    ) {
+        return await reservationRepository.find({
+            where: { status: 'pending' },
+        });
     }
 
     static async updateReservationsStatus(
-        id: string, updateReservationsStatus: 
-        UpdateReservationsStatusDto, reservationRepository: 
-        Repository<Reservation>) {
-
-        const existingReservation = await reservationRepository.findOneBy({ id: +id })
-        if (!existingReservation || existingReservation.status === updateReservationsStatus.status) {
-            return false
+        id: string,
+        updateReservationsStatus: UpdateReservationsStatusDto,
+        reservationRepository: Repository<Reservation>,
+    ) {
+        const existingReservation = await reservationRepository.findOneBy({
+            id: +id,
+        });
+        if (
+            !existingReservation ||
+            existingReservation.status === updateReservationsStatus.status
+        ) {
+            return false;
         }
-        existingReservation.status = updateReservationsStatus.status
-        return await reservationRepository.save(existingReservation)
+        existingReservation.status = updateReservationsStatus.status;
+        return await reservationRepository.save(existingReservation);
     }
 
-
-
     static async getBusinessEvents(
-        businessEvent: CreateBusinessEventDto, 
-        businessEventRepository: Repository<BusinessEvent>, 
-        businessRepository: Repository<Business>){
-
-        const business = await DataBaseHelper.getBusinessesByID(`${businessEvent.businessId}`, businessRepository )
-        if(!business){
-            return false
+        businessEvent: CreateBusinessEventDto,
+        businessEventRepository: Repository<BusinessEvent>,
+        businessRepository: Repository<Business>,
+    ) {
+        const business = await DataBaseHelper.getBusinessesByID(
+            `${businessEvent.businessId}`,
+            businessRepository,
+        );
+        if (!business) {
+            return false;
         }
-        return await businessEventRepository.find({where:{businessId: businessEvent.businessId}})
+        return await businessEventRepository.find({
+            where: { businessId: businessEvent.businessId },
+        });
     }
 
     static async createUniqueBusinessEvents(
-        businessEvent: CreateBusinessEventDto, 
-        businessEventRepository: Repository<BusinessEvent>, 
-        businessRepository: Repository<Business>): Promise<false | BusinessEvent> {
-
-        businessEvent.visitorsCount = 0
-        const business = await DataBaseHelper.getBusinessesByID(`${businessEvent.businessId}`, businessRepository)        
-        if(!business){
-            return false
+        businessEvent: CreateBusinessEventDto,
+        businessEventRepository: Repository<BusinessEvent>,
+        businessRepository: Repository<Business>,
+    ): Promise<false | BusinessEvent> {
+        businessEvent.visitorsCount = 0;
+        const business = await DataBaseHelper.getBusinessesByID(
+            `${businessEvent.businessId}`,
+            businessRepository,
+        );
+        if (!business) {
+            return false;
         }
-        const createdBusinessEvent = businessEventRepository.create(businessEvent)
-        return businessEventRepository.save(createdBusinessEvent)
+        const createdBusinessEvent =
+            businessEventRepository.create(businessEvent);
+        return businessEventRepository.save(createdBusinessEvent);
     }
 
     static async incrementBusinessEventUserCounter(
-        businessEventId: string, 
-        businessEventRepository: Repository<BusinessEvent>){
-
-        const businessEvent = await businessEventRepository.findOneBy({id:+businessEventId})
-        if(!businessEvent){
-            return false
+        businessEventId: string,
+        businessEventRepository: Repository<BusinessEvent>,
+    ) {
+        const businessEvent = await businessEventRepository.findOneBy({
+            id: +businessEventId,
+        });
+        if (!businessEvent) {
+            return false;
         }
-        businessEvent.visitorsCount+=1
-        return businessEventRepository.save(businessEvent)
+        businessEvent.visitorsCount += 1;
+        return businessEventRepository.save(businessEvent);
     }
 }
--- src/helpers/email.helper.ts
+++ src/helpers/email.helper.ts
@@ -1,29 +1,52 @@
-import { MailerService } from "@nestjs-modules/mailer";
-import { EmailScrapingDataDto } from "../scraper/dto/email-scraping-data.dto";
-import { CreateReservationDto } from "../reservation/dto/create-reservation.dto";
-
+import { MailerService } from '@nestjs-modules/mailer';
+import { EmailScrapingDataDto } from '../scraper/dto/email-scraping-data.dto';
+import { CreateReservationDto } from '../reservation/dto/create-reservation.dto';
 
 export class EmailHelper {
-    async sendReservationEmail(mailerService: MailerService, updateReservation: CreateReservationDto): Promise<void> {
+    async sendReservationEmail(
+        mailerService: MailerService,
+        updateReservation: CreateReservationDto,
+    ): Promise<void> {
         const templates = {
             confirmed: `Your reservation was confirmed\nDate: ${updateReservation.date}.\nTime: ${updateReservation.time}`,
-            declined: `Your reservation was declined\nDate: ${updateReservation.date}.\nTime: ${updateReservation.time}`
-        }
-        const from = 'scraper.api.study@gmail.com'
-        const subject = "Reservation"
-        await this.sendEmail(mailerService, from, subject, updateReservation.email, templates[updateReservation.status])
+            declined: `Your reservation was declined\nDate: ${updateReservation.date}.\nTime: ${updateReservation.time}`,
+        };
+        const from = 'scraper.api.study@gmail.com';
+        const subject = 'Reservation';
+        await this.sendEmail(
+            mailerService,
+            from,
+            subject,
+            updateReservation.email,
+            templates[updateReservation.status],
+        );
     }
 
-    async sendScrapingResultEmail(mailerService: MailerService, emailData: EmailScrapingDataDto): Promise<void>{
+    async sendScrapingResultEmail(
+        mailerService: MailerService,
+        emailData: EmailScrapingDataDto,
+    ): Promise<void> {
         const templates = {
             scrapingError: `Scraping error: something went wrong :(\nBefore error scraped: ${emailData.businessesCount} businesses, city: ${emailData.city}`,
             scrapingExistingCity: `This city has been already scraped: ${emailData.city}`,
-            success:  `Successfully scraped: ${emailData.businessesCount} businesses, city: ${emailData.city}`
-        }
-        await this.sendEmail(mailerService, emailData.from, emailData.subject, emailData.email, templates[emailData.status])
+            success: `Successfully scraped: ${emailData.businessesCount} businesses, city: ${emailData.city}`,
+        };
+        await this.sendEmail(
+            mailerService,
+            emailData.from,
+            emailData.subject,
+            emailData.email,
+            templates[emailData.status],
+        );
     }
 
-    async sendEmail(mailerService: MailerService, from: string, subject: string, to: string, text: string): Promise<void>{
-        await mailerService.sendMail({ to, from, subject, text })
+    async sendEmail(
+        mailerService: MailerService,
+        from: string,
+        subject: string,
+        to: string,
+        text: string,
+    ): Promise<void> {
+        await mailerService.sendMail({ to, from, subject, text });
     }
 }
